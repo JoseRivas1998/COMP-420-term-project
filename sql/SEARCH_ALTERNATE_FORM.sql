56230b606df69bf5731f2ae3f9a151714f536bdd
Kylie G <godinezkylie@gmail.com>
2020-11-29 23:42:38 -0800

    Search alternative forms

---
+++ sql/SEARCH_ALTERNATE_FORM.sql
@@ -0,0 +1,15 @@
+-- A report will be generated of a Pokemon that lists it’s alternative forms, including the changes to type, ability, and base stats.
+USE pokemondb;
+DROP PROCEDURE IF EXISTS alternative_forms;
+
+DELIMITER //
+CREATE PROCEDURE alternative_forms(IN POKEMON_NUM INT(11))
+BEGIN
+	SELECT
+    POKEMON.POKEMON_NAME AS "POKEMON",
+    ALTERNATE_FORM.ALTERNATE_FORM_NAME AS "ALTERNATE FORM NAME"
+    FROM POKEMON
+    INNER JOIN ALTERNATE_FORM ON ALTERNATE_FORM.POKEMON_ID=POKEMON.POKEMON_ID
+	WHERE POKEMON.POKEMON_ID = POKEMON_NUM;
+END //
+DELIMITER ;
