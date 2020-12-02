016d0aafba08cf920f55b6dbbf79d5a6580695bd
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2020-12-01 19:25:02 -0800

    Type repo

--- src/main/java/edu/csuci/comp420term/data/mysqlrepos/MySQLTypeRepository.java
+++ src/main/java/edu/csuci/comp420term/data/mysqlrepos/MySQLTypeRepository.java
@@ -1,18 +1,73 @@
 package edu.csuci.comp420term.data.mysqlrepos;
 
+import edu.csuci.comp420term.data.ConnectionBuilder;
 import edu.csuci.comp420term.entities.Type;
 import edu.csuci.comp420term.repos.TypeRepository;
+import edu.csuci.comp420term.utils.EntityCache;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public final class MySQLTypeRepository implements TypeRepository {
-    @Override
-    public Type findById(int id) {
-        return null;
+
+    private static final String SELECT_TYPE_BY_ID = "SELECT * FROM TYPE WHERE TYPE_ID = ? LIMIT 1";
+    private static final String SELECT_ALL_TYPES = "SELECT * FROM TYPE ORDER BY TYPE_ID";
+    private final EntityCache<Integer, Type> typeCache;
+
+    public MySQLTypeRepository() {
+        typeCache = new EntityCache<>();
     }
 
     @Override
-    public List<Type> findAll() {
-        return null;
+    public Type findById(int id) throws SQLException {
+        final Optional<Type> cachedType = typeCache.get(id);
+        if (cachedType.isPresent()) {
+            return cachedType.get();
+        }
+        try (Connection connection = ConnectionBuilder.buildConnection();
+             PreparedStatement selectStatement = connection.prepareStatement(SELECT_TYPE_BY_ID)) {
+            selectStatement.setInt(1, id);
+            try (ResultSet resultSet = selectStatement.executeQuery()) {
+                verifyTypeExistsInResultSet(id, resultSet);
+                final Type type = buildTypeFromResultSet(resultSet);
+                cacheType(type);
+                return type;
+            }
+        }
+    }
+
+    private Type buildTypeFromResultSet(ResultSet resultSet) throws SQLException {
+        final int typeId = resultSet.getInt("TYPE_ID");
+        final String typeName = resultSet.getString("TYPE_NAME");
+        final String typeColorHex = "#" + resultSet.getString("TYPE_COLOR_HEX");
+        return new Type(typeId, typeName, typeColorHex);
+    }
+
+    private void verifyTypeExistsInResultSet(int id, ResultSet resultSet) throws SQLException {
+        if (!resultSet.next()) throw new SQLException(String.format("The type with id %d can not be found", id));
+    }
+
+    @Override
+    public List<Type> findAll() throws SQLException {
+        try (Connection connection = ConnectionBuilder.buildConnection();
+             PreparedStatement selectStatement = connection.prepareStatement(SELECT_ALL_TYPES);
+             ResultSet resultSet = selectStatement.executeQuery()) {
+            List<Type> types = new ArrayList<>();
+            while (resultSet.next()) {
+                final Type type = buildTypeFromResultSet(resultSet);
+                cacheType(type);
+                types.add(type);
+            }
+            return types;
+        }
+    }
+
+    private void cacheType(Type type) {
+        typeCache.cache(type.id, type);
     }
 }
--- src/main/java/edu/csuci/comp420term/repos/TypeRepository.java
+++ src/main/java/edu/csuci/comp420term/repos/TypeRepository.java
@@ -2,12 +2,12 @@ package edu.csuci.comp420term.repos;
 
 import edu.csuci.comp420term.entities.Type;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface TypeRepository {
 
-    Type findById(final int id);
-    List<Type> findAll();
-    
+    Type findById(final int id) throws SQLException;
+    List<Type> findAll() throws SQLException;
 
 }
