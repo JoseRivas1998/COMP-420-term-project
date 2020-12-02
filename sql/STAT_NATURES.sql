15289733dc81b15924cf012ad934f0353bd8078a
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2020-12-01 19:20:03 -0800

    Stat Natures stored procedure

---
+++ sql/STAT_NATURES.sql
@@ -0,0 +1,29 @@
+USE pokemondb;
+
+DROP PROCEDURE IF EXISTS get_natures_that_increase_stat;
+DROP PROCEDURE IF EXISTS get_natures_that_decrease_stat;
+
+DELIMITER //
+
+CREATE PROCEDURE get_natures_that_increase_stat(IN STAT INT)
+BEGIN
+    SELECT NATURE.*
+    FROM NATURE_STAT
+             JOIN NATURE USING (NATURE_ID)
+    WHERE STAT_ID = STAT
+      AND STAT_MULTIPLIER > 1;
+END //
+CREATE PROCEDURE get_natures_that_decrease_stat(IN STAT INT)
+BEGIN
+    SELECT NATURE.*
+    FROM NATURE_STAT
+             JOIN NATURE USING (NATURE_ID)
+    WHERE STAT_ID = STAT
+      AND STAT_MULTIPLIER < 1;
+
+END //
+
+DELIMITER ;
+
+CALL get_natures_that_increase_stat(5);
+CALL get_natures_that_decrease_stat(5);
