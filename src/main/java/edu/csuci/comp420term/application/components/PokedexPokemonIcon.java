205247bf812abb8d74f7c799371fd7f4579b6611
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2020-12-03 09:58:10 -0800

    Added type to pokemon Icon

--- src/main/java/edu/csuci/comp420term/application/components/PokedexPokemonIcon.java
+++ src/main/java/edu/csuci/comp420term/application/components/PokedexPokemonIcon.java
@@ -3,6 +3,7 @@ package edu.csuci.comp420term.application.components;
 import edu.csuci.comp420term.application.ApplicationContext;
 import edu.csuci.comp420term.application.containers.PokemonPage;
 import edu.csuci.comp420term.entities.Pokemon;
+import edu.csuci.comp420term.entities.Type;
 import javafx.beans.property.Property;
 import javafx.geometry.HPos;
 import javafx.scene.control.Label;
@@ -10,8 +11,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PokedexPokemonIcon extends GridPane {
 
@@ -36,7 +42,18 @@ public class PokedexPokemonIcon extends GridPane {
     private void addPokemonName(Pokemon pokemon) {
         final Label nameLabel = new Label(pokemon.name);
         nameLabel.setStyle("-fx-font-size: 2em");
-        this.add(nameLabel, 0, 2);
+        final HBox nameBox = new HBox(ApplicationContext.SPACING / 2);
+        nameBox.getChildren().addAll(nameLabel);
+        final List<Type> types = getTypeList(pokemon);
+        nameBox.getChildren().addAll(types.stream().map(TypeLabel::new).collect(Collectors.toList()));
+        this.add(nameBox, 0, 2);
+    }
+
+    private List<Type> getTypeList(Pokemon pokemon) {
+        final List<Type> types = new ArrayList<>();
+        types.add(pokemon.primaryType);
+        pokemon.getSecondaryType().ifPresent(types::add);
+        return types;
     }
 
     private void addPokemonIcon(Pokemon pokemon) {
