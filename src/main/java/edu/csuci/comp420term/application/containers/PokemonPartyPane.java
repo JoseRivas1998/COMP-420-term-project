61814e2016d7a8b8191a8fb604a5cb09658da9df
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2020-12-03 09:51:21 -0800

    Pokemon Party Controls

--- src/main/java/edu/csuci/comp420term/application/containers/PokemonPartyPane.java
+++ src/main/java/edu/csuci/comp420term/application/containers/PokemonPartyPane.java
@@ -4,8 +4,11 @@ import edu.csuci.comp420term.application.ApplicationContext;
 import edu.csuci.comp420term.application.components.PokedexPokemonIcon;
 import edu.csuci.comp420term.application.dialogs.ErrorDialog;
 import edu.csuci.comp420term.entities.Pokemon;
+import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
@@ -54,16 +57,42 @@ public class PokemonPartyPane extends BorderPane {
 
     private void updateGrid(List<Pokemon> pokemonList) {
         GridPane partyGrid = new GridPane();
-        partyGrid.setGridLinesVisible(true);
+//        partyGrid.setGridLinesVisible(true);
         partyGrid.setVgap(ApplicationContext.SPACING);
         partyGrid.setHgap(ApplicationContext.SPACING);
         for (int i = 0; i < pokemonList.size(); i++) {
-            Pokemon pokemon = pokemonList.get(i);
+            final Pokemon pokemon = pokemonList.get(i);
             final int row = i / 3;
             final int column = i % 3;
             final PokedexPokemonIcon pokedexPokemonIcon = new PokedexPokemonIcon(pokemon);
+
+            final Button moveBack = new Button("<");
+            final Button moveForward = new Button(">");
+            final Button remove = new Button("X");
+
+            moveBack.setOnAction(event -> movePokemonBackAsync(pokemon, moveBack, moveForward, remove));
+            moveForward.setOnAction(event -> moveForwardAsync(pokemon, moveBack, moveForward, remove));
+            remove.setOnAction(event -> {
+                removePokemonFromPartyAsync(pokemon, moveBack, moveForward, remove);
+            });
+
+            final GridPane partyItem = new GridPane();
+            partyItem.setHgap(ApplicationContext.SPACING / 2);
+            partyItem.setVgap(ApplicationContext.SPACING / 2);
             GridPane.setHgrow(pokedexPokemonIcon, Priority.ALWAYS);
-            partyGrid.add(pokedexPokemonIcon, column, row);
+            GridPane.setHgrow(moveBack, Priority.ALWAYS);
+            GridPane.setHgrow(moveForward, Priority.ALWAYS);
+            GridPane.setColumnSpan(pokedexPokemonIcon, 2);
+            GridPane.setHalignment(moveForward, HPos.RIGHT);
+            GridPane.setHgrow(remove, Priority.ALWAYS);
+            GridPane.setHalignment(remove, HPos.RIGHT);
+            partyItem.add(remove, 1, 0);
+            partyItem.add(pokedexPokemonIcon, 0, 1);
+            partyItem.add(moveBack, 0, 2);
+            partyItem.add(moveForward, 1, 2);
+
+            GridPane.setHgrow(partyItem, Priority.ALWAYS);
+            partyGrid.add(partyItem, column, row);
         }
         for (int i = 0; i < 3; i++) {
             final ColumnConstraints columnConstraints = new ColumnConstraints();
@@ -71,6 +100,90 @@ public class PokemonPartyPane extends BorderPane {
             partyGrid.getColumnConstraints().add(columnConstraints);
         }
         this.setCenter(partyGrid);
+    }
+
+    private void removePokemonFromPartyAsync(Pokemon pokemon, Button moveBack, Button moveForward, Button remove) {
+        updateDisabledForButtons(moveBack, moveForward, remove, true);
+        new Thread(() -> {
+            removePokemonFromParty(pokemon, moveBack, moveForward, remove);
+        }).start();
+    }
+
+    private void removePokemonFromParty(Pokemon pokemon, Button moveBack, Button moveForward, Button remove) {
+        final ApplicationContext appContext = ApplicationContext.appContext();
+        try {
+            final String username = appContext.userRepo.currentUser();
+            appContext.partyRepo.removeFromParty(username, pokemon);
+            Platform.runLater(() -> {
+                this.refreshPartyPane();
+                updateDisabledForButtons(moveBack, moveForward, remove, false);
+            });
+        } catch (Exception exception) {
+            Platform.runLater(() -> {
+                ErrorDialog errorDialog = new ErrorDialog(exception);
+                errorDialog.initOwner(appContext.mainStage);
+                errorDialog.showAndWait();
+                updateDisabledForButtons(moveBack, moveForward, remove, false);
+            });
+        }
+    }
+
+    private void moveForwardAsync(Pokemon pokemon, Button moveBack, Button moveForward, Button remove) {
+        updateDisabledForButtons(moveBack, moveForward, remove, true);
+        new Thread(() -> {
+            moveForward(pokemon, moveBack, moveForward, remove);
+        }).start();
+    }
+
+    private void moveForward(Pokemon pokemon, Button moveBack, Button moveForward, Button remove) {
+        final ApplicationContext appContext = ApplicationContext.appContext();
+        try {
+            final String username = appContext.userRepo.currentUser();
+            appContext.partyRepo.moveForward(username, pokemon);
+            Platform.runLater(() -> {
+                this.refreshPartyPane();
+                updateDisabledForButtons(moveBack, moveForward, remove, false);
+            });
+        } catch (Exception exception) {
+            Platform.runLater(() -> {
+                ErrorDialog errorDialog = new ErrorDialog(exception);
+                errorDialog.initOwner(appContext.mainStage);
+                errorDialog.showAndWait();
+                updateDisabledForButtons(moveBack, moveForward, remove, false);
+            });
+        }
+    }
+
+    private void movePokemonBackAsync(Pokemon pokemon, Button moveBack, Button moveForward, Button remove) {
+        updateDisabledForButtons(moveBack, moveForward, remove, true);
+        new Thread(() -> {
+            movePokemonBack(pokemon, moveBack, moveForward, remove);
+        }).start();
+    }
+
+    private void movePokemonBack(Pokemon pokemon, Button moveBack, Button moveForward, Button remove) {
+        final ApplicationContext appContext = ApplicationContext.appContext();
+        try {
+            final String username = appContext.userRepo.currentUser();
+            appContext.partyRepo.moveBack(username, pokemon);
+            Platform.runLater(() -> {
+                this.refreshPartyPane();
+                updateDisabledForButtons(moveBack, moveForward, remove, false);
+            });
+        } catch (Exception exception) {
+            Platform.runLater(() -> {
+                ErrorDialog errorDialog = new ErrorDialog(exception);
+                errorDialog.initOwner(appContext.mainStage);
+                errorDialog.showAndWait();
+                updateDisabledForButtons(moveBack, moveForward, remove, false);
+            });
+        }
+    }
+
+    private void updateDisabledForButtons(Button moveBack, Button moveForward, Button remove, boolean disable) {
+        moveBack.setDisable(disable);
+        moveForward.setDisable(disable);
+        remove.setDisable(disable);
     }
 
 }
